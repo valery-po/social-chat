7ebe456cc30d0b4ef6a8502713bf390a046152ad
valery-po <54510559+valery-po@users.noreply.github.com>
2020-06-06 00:25:35 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+
+const menuBtn = document.querySelector(".m-menu");
+const menu = document.querySelector(".menu-sidebar");
+
+
+
+menuBtn.addEventListener("click", function() {
+	
+	menu.classList.toggle("active");
+	
+});
